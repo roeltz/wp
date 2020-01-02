6d54de7d254362ea6d6ed0b524f5bb51b6b340d5
Leonardo Rothe Tagliafico <leonardo.rothe@shiftactive.com>
2020-01-02 12:27:17 -0500

    New AdminNotice class for custom notifications

---
+++ src/Roeltz/Wordpress/Admin/AdminNotice.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Roeltz\Wordpress\Admin;
+use Roeltz\HTML\HTML;
+use Roeltz\Wordpress\Admin;
+
+class AdminNotice {
+
+	static function create($class, $content, $deferUntilPage = null) {
+		if ($deferUntilPage) {
+			if (preg_match('#[\w+]+:\w+$#', $deferUntilPage))
+				$deferUntilPage = Admin::actionToSlug($deferUntilPage);
+	
+			$_SESSION["custom-admin-notices"][$deferUntilPage][] = compact("class", "content");
+		} else {
+			add_action("custom_admin_notices", function() use($class, $content){
+				echo HTML::tag("div", ["class"=>"notice notice-$class"], [
+					HTML::tag("p", [], [$content])
+				]);
+			});
+		}
+	}
+	
+	static function info($content, $deferUntilPage = null) {
+		self::create("info", $content, $deferUntilPage);
+	}
+	
+	static function success($content, $deferUntilPage = null) {
+		self::create("success", $content, $deferUntilPage);
+	}
+	
+	static function warning($content, $deferUntilPage = null) {
+		self::create("warning", $content, $deferUntilPage);
+	}
+	
+	static function error($content, $deferUntilPage = null) {
+		self::create("error", $content, $deferUntilPage);
+	}
+	
+	static function put() {
+		if ($page = @$_GET["page"]) {
+			$deferred = $_SESSION["custom-admin-notices"][$page];
+	
+			foreach ($deferred as $notice)
+				self::create($notice["class"], $notice["content"]);
+	
+			unset($_SESSION["custom-admin-notices"][$page]);
+		}
+	
+		do_action("custom_admin_notices");
+	}
+	
+}
